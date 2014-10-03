8933ec267a456a81ad775f49acb84d9262c64624
Eric Pascual <eric.pascual@cstb.fr>
2014-10-03 18:48:24 +0200

    added put and delete handlers tests

--- test/test_ws.py
+++ test/test_ws.py
@@ -38,7 +38,51 @@ class TestWebServices(unittest.TestCase):
         r = requests.get(self.URL_BASE + "/scenario/s42/settings")
         self.assertEqual(r.status_code, 404)
 
-    def test04_execute_scenario(self):
+    def test03_set_scenario_settings(self):
+        url = self.URL_BASE + "/scenario/s01/settings"
+        r = requests.get(url)
+        self.assertEqual(r.status_code, 200)
+
+        data = ReplyData(r.json())
+        saved_label = data.label
+        data.label = new_label = 'modified test scenario 01'
+
+        r = requests.post(url, data=json.dumps(data.__dict__))
+        self.assertEqual(r.status_code, 200)
+
+        r = requests.get(url)
+        self.assertEqual(r.status_code, 200)
+        data = ReplyData(r.json())
+        self.assertEqual(data.label, new_label)
+
+        data.label = saved_label
+        r = requests.post(url, data=json.dumps(data.__dict__))
+        self.assertEqual(r.status_code, 200)
+        r = requests.get(url)
+        self.assertEqual(r.status_code, 200)
+        data = ReplyData(r.json())
+        self.assertEqual(data.label, saved_label)
+
+    def test04_add_remove_scenario_settings(self):
+        url = self.URL_BASE + "/scenario/s01/settings"
+        r = requests.get(url)
+
+        data = ReplyData(r.json())
+        data.label = 'copy of ' + data.label
+        r = requests.put(self.URL_BASE + "/scenario/s03/settings", data=json.dumps(data.__dict__))
+        self.assertEqual(r.status_code, 200)
+
+        r = requests.get(self.URL_BASE + "/scenario/s03/settings", data=json.dumps(data.__dict__))
+        self.assertEqual(r.status_code, 200)
+
+        r = requests.delete(self.URL_BASE + "/scenario/s03/settings")
+        self.assertEqual(r.status_code, 200)
+
+        url = self.URL_BASE + "/scenario/s03/settings"
+        r = requests.get(url)
+        self.assertEqual(r.status_code, 404)
+
+    def test10_execute_scenario(self):
         r = requests.get(self.URL_BASE + "/scenario/s01/execute")
         self.assertEqual(r.status_code, 200)
 
